dd2f24b2e7a67c7b94d631ab67f46e3a66b400b5
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-21 01:09:04 -0300

    Criando o local onde vai ficar as imgs

--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -2,11 +2,23 @@ import styles from '../home/header.module.scss'
 
 const Header = () => {
 
+  const iconsAnime = [
+    {id: 1, img: ''},
+    {id: 2, img: ''},
+    {id: 3, img: ''},
+    {id: 4, img: ''},
+    {id: 5, img: ''}
+  ]
+
     
   return (
     <>
     <header className={styles.header}>
       <section className={styles.icons_anime}>
+        {/* tenho que pegar as img dos icons  */}
+        {iconsAnime.map((iconImg) => (
+          <div key={iconImg.id}></div>
+        ))}
 
       </section>
     </header>
