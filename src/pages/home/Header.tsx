d887f4668a52efcb502f9663e60fe9cf466032f8
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-21 01:11:20 -0300

    Estilizando da maneira que deveria ter feito antes

--- src/pages/home/Header.tsx
+++ src/pages/home/Header.tsx
@@ -14,13 +14,13 @@ const Header = () => {
   return (
     <>
     <header className={styles.header}>
-      <section className={styles.icons_anime}>
+      <ul className={styles.icons_anime}>
         {/* tenho que pegar as img dos icons  */}
         {iconsAnime.map((iconImg) => (
-          <div key={iconImg.id}></div>
+          <li key={iconImg.id}>{iconImg.img}</li>
         ))}
 
-      </section>
+      </ul>
     </header>
 
     </>
