ba6e4cff2cd12280b02f17cd4787e47cfbbc8d3b
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-21 11:25:12 -0300

    ok para a troca de páginas

--- src/pages/scenario/AnimeExhibition.tsx
+++ src/pages/scenario/AnimeExhibition.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import styles from '../scenario/animeExhibition.module.scss'
 import { AiFillHome } from "react-icons/ai";
+import App from '../../App';
 
 
-const [homePage, setHomePage] = useState<boolean>(false)
+
 
 const AnimeExhibition = () => {
+
+const [homePage, setHomePage] = useState<boolean>(false)
+if(homePage) {
+  return <App />
+}
 
   return (
     <>
     <main className={styles.anime_main}>
-      <header><AiFillHome /></header>
+      <header className={styles.home_page_return}><AiFillHome 
+       className={styles.home_icon} onClick={() => setHomePage(!homePage)} color='white' size={45}/></header>
+
 
     </main>
     </>
