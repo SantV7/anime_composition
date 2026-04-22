00fdb9e17400191d495563d1aa663ffcf3930fd6
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-21 23:28:11 -0300

    Importando as imgs pro elemento pai

--- src/pages/scenario/AnimeExhibition.tsx
+++ src/pages/scenario/AnimeExhibition.tsx
@@ -4,9 +4,30 @@ import { AiFillHome } from "react-icons/ai";
 import gsap from 'gsap';
 import Cards from './Cards';
 
+// imgs do slider
+
+import denji from '../../assets/img/slider_img/denji.jpg'
+import gojo_c from '../../assets/img/slider_img/gojo-c.jpg'
+import touka from '../../assets/img/slider_img/touka.jpg'
+import eren from '../../assets/img/slider_img/eren.jpg'
+import demonSlayer from '../../assets/img/slider_img/ds-reflex.jpg'
+import friren_j from '../../assets/img/slider_img/frieren-jumping.jpg'
+import gojo_s from '../../assets/img/slider_img/gojo-s.jpg'
+import raze from '../../assets/img/slider_img/raze.jpg'
+import kaneki from '../../assets/img/slider_img/kanenki-ken.jpg'
+import frieren_s from '../../assets/img/slider_img/frieren-s.jpg'
+import att_flower from '../../assets/img/slider_img/att.jpg'
+
+
+
+
+// INTERFACES
+
 interface Props {
   onBack: () => void;
 }
+
+
 
 const AnimeExhibition = ({ onBack }: Props) => {
   const mainHome = useRef<HTMLElement>(null);
@@ -34,6 +55,8 @@ const AnimeExhibition = ({ onBack }: Props) => {
       });
     }
   }, []);
+
+  
 
   return (
     <main ref={mainHome} className={styles.anime_main}>
