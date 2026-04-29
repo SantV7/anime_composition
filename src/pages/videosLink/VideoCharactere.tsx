0bc2822aee3ac0bddcf58160ac788bb2f538dd6c
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-04-28 22:13:37 -0300

    Depois vou adicionar outros videos

--- src/pages/videosLink/VideoCharactere.tsx
+++ src/pages/videosLink/VideoCharactere.tsx
@@ -2,9 +2,14 @@ import styles from './videoCharactere.module.scss'
 const VideoCharactere = () => {
 
 
+
+
   return (
     <>
       <section className={styles.area_video}>
+        <iframe className={styles.iframe_config} rel='0'  src="https://www.youtube.com/embed/-xt1y8Ldsos?si=OGZJHOlsiGOHkJMX" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+
+        <iframe className={styles.iframe_config} rel='0'  src="https://www.youtube.com/embed/LjJeYKnaU6E?si=ivPWT6hPdLxvOYLy" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
       </section>
     </>
